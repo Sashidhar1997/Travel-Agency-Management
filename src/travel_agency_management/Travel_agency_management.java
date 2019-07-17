da418d9fafa905ece92dd686b762380775d74560
Sashidhar1997 <ihsashidharreddy1997@gmail.com>
2019-07-17 23:28:56 +0530

    Commited

---
+++ src/travel_agency_management/Travel_agency_management.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travel_agency_management;
+
+/**
+ *
+ * @author sashidhar
+ */
+public class Travel_agency_management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainScreen m=new MainScreen();
+        m.setVisible(true);
+    }
+    
+}
